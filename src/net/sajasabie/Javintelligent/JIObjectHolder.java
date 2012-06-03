f6dd416dc18cd45810b43fb30452ca1ba01bc7a2
Anvil0008 <chris@home>
2012-06-03 12:57:54 -0700

    revisions of everything

---
+++ src/net/sajasabie/Javintelligent/JIObjectHolder.java
@@ -0,0 +1,52 @@
+package net.sajasabie.Javintelligent;
+
+public class JIObjectHolder {
+	public JIObject mObject;
+	public JIRenderable mRenderable;
+	public double mX = 0;
+	public double mY = 0;
+	
+	public JIObjectHolder(JIObject contents ,  JIRenderable contentRenderable) {
+		this.mObject = contents;
+		this.mRenderable = contentRenderable;
+		if(this.mRenderable != null) JIApplication.getRenderer().attach(this.mRenderable);
+	}
+	
+	public void onMoveUpdate() {
+		if(this.mObject != null) this.mObject.update();
+	}
+	
+	public void onRenderableUpdate() {
+		if(this.mRenderable != null) this.mRenderable.setPosition(this.getX()*600, this.getY()*600);
+	}
+	
+	public void setPosition(double pX, double pY) {
+		this.mX = pX;
+		this.mY = pY;
+	}
+	
+	public void setX(double pX) {
+		this.setPosition(pX, this.getY());
+	}
+	
+	public void setY(double pY) {
+		this.setPosition(this.getX(), pY);
+	}
+	
+	public double getX() {
+		return this.mX;
+	}
+	
+	public double getY() {
+		return this.mY;
+	}
+	
+	public JIObject getObject() {
+		return this.mObject;
+	}
+	
+	public JIRenderable getRenderable() {
+		return this.mRenderable;
+	}
+	
+}

c93b2f7b47dbf988ad35a022d520ddcc26a2f40e
kingnai <iangoegebuer@gmail.com>
2012-06-03 00:20:14 -0700

    Move command with feedback added

--- src/net/sajasabie/Javintelligent/JIWorldTest.java
+++ src/net/sajasabie/Javintelligent/JIWorldTest.java
@@ -3,8 +3,14 @@ package net.sajasabie.Javintelligent;
 
 
 public class JIWorldTest {
-	public static void main(String argv[]) {
+	public static void main(String[] argv) {
 		JIWorld theWorldTest = new JIWorld();
+		System.out.println(theWorldTest);
+		
+		System.out.println(theWorldTest.move(0.02f,0.02f));
+		System.out.println(theWorldTest);
+		
+		System.out.println(theWorldTest.move(-0.02f,-0.02f));
 		System.out.println(theWorldTest);
 	}
 }

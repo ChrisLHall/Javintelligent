626b3b80be49ad926e5bdf8babcb76760d60f804
kingnai <iangoegebuer@gmail.com>
2012-06-03 05:30:43 -0700

    Pushing Ians

--- src/net/sajasabie/Javintelligent/IanJIObject.java
+++ src/net/sajasabie/Javintelligent/IanJIObject.java
@@ -7,7 +7,9 @@ public class IanJIObject extends JIObject {
 	
 	double dx = 0.0;
 	double dy = 0.0;
-	
+	double accel = 0.0005;
+	double speed = 0.005;
+	double maxspeed = 0;
 	int state = 0;
 	
 	JIErrors lasterrors[] = {JIErrors.NONE,JIErrors.NONE,JIErrors.NONE};
@@ -19,6 +21,63 @@ public class IanJIObject extends JIObject {
 	
 	@Override
 	public void update() {
+		if(error == JIErrors.YOUWIN) {
+			state = 0;
+			System.out.println("\r\r\r\r\r\r\r\r\r");
+		}
+		if(state == 0) {
+			dP[0] = 0;
+			dP[1] = 0;
+			state++;
+		} else if(state == 1) {
+			dP[0] = .003;
+			state++;
+		} else if(state == 2) {
+			dP[0] = 0;
+			dx = change;
+			state++;
+		} else if(state == 3) {
+			dP[1] = .003;
+			state++;
+		} else if(state == 4) {
+			dP[1] = 0;
+			dy = change;
+			state++;
+		} else if(state == 5){
+			dP[0] = -.003;
+			dP[1] = -.003;
+			state++;
+		} else if(state == 5){
+			dP[0] = 0;
+			dP[1] = 0;
+			state++;
+		} else if(error == JIErrors.OOB) {
+			state = 0;
+		} else if(error == JIErrors.TOOFAR) {
+			maxspeed = speed-accel;
+			state = 0;
+		} else {
+			if(dy < 0) System.out.println("It's up!");
+			else System.out.println("It's down!");
+			if(dx > 0) System.out.println("It's right!");
+			else System.out.println("It's left!");
+			System.out.println(error);
+			System.out.println(state);
+			System.out.println(change);
+			double angle = Math.atan(Math.sqrt(dy*dy)/Math.sqrt(dx*dx));
+			
+			//if(error != JIErrors.TOOFAR && state == 6 && change >= 0) {
+				speed += accel;
+				if(maxspeed != 0) speed = maxspeed;
+				dP[1] = Math.sin(angle)*speed*((dy < 0)? -1:1);
+				dP[0] = Math.cos(angle)*speed*((dx < 0)? -1:1);
+				System.out.println("WARPSPEEEEEED");
+		}
+		
+		
+		
+		
+	/*
 		if(error == JIErrors.YOUWIN || error == JIErrors.YOULOSE) turns = 0;
 		
 		
@@ -71,7 +130,7 @@ public class IanJIObject extends JIObject {
 				
 		}
 			
-		turns++;
+		turns++;*/
 	}
 	
 }

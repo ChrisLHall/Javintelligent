569763e46e079cf6b1378d8c0331382fbd5d7dcb
Anvil0008 <chris@home>
2012-06-03 02:26:59 -0700

    committing buildandrun

---
+++ bin/net/sajasabie/Javintelligent/buildandrun.bat
@@ -0,0 +1,5 @@
+cd %USERPROFILE%\Documents\Javintel\src\net\sajasabie\Javintelligent
+javac -d ..\..\..\..\bin *.java
+cd %USERPROFILE%\Documents\Javintel\bin
+java net.sajasabie.Javintelligent.JIWorldTest > test.out
+cd %USERPROFILE%\Documents\Javintel\src\net\sajasabie\Javintelligent

c94d983cbbe0f577a19316c288f3af0944e8ba15
Anvil0008 <chris@home>
2012-06-03 00:55:02 -0700

    Updated renderer

---
+++ src/net/sajasabie/Javintelligent/JIObject.java
@@ -0,0 +1,7 @@
+package net.sajasabie.Javintelligent;
+
+public class JIObject {
+	public double dP[] = {0.0,0.0};
+	public enum Errors {TOOFAR,TOOSHORT,OUTOFBOUNDS,NONE,YOUWIN};
+	public Errors error = Errors.NONE;
+}

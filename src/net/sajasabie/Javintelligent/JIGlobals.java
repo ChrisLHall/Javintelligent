659a8869d8a1496f221d96da054640c908309d37
kingnai <iangoegebuer@gmail.com>
2012-06-03 12:53:34 -0700

    Sending globals

--- src/net/sajasabie/Javintelligent/JIGlobals.java
+++ src/net/sajasabie/Javintelligent/JIGlobals.java
@@ -3,4 +3,7 @@ package net.sajasabie.Javintelligent;
 public class JIGlobals {
 	public double width = 1.0;
 	public double height = 1.0;
+	public double maxSpeed = 0.005;
+	public double vWidth = 600;
+	public double vHeight = 600;
 }

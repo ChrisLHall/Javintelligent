8384fe3963a232ca6a746f75e08952ae50f7a621
unknown <iangoegebuer@gmail.com>
2012-06-02 22:41:31 -0700

    Added array generator

--- src/net/sajasabie/Javintelligent/JIWorld.java
+++ src/net/sajasabie/Javintelligent/JIWorld.java
@@ -1,10 +1,15 @@
+import java.util.Random;
+
 package net.sajasabie.Javintelligent;
+
+
 
 public class JIWorld {
 	
-	char *map[][] = new char[100][100];
+	char map[][] = new char[100][100];
 	
 	public JIWorld() {
+		genWorld();
 	}
 	
 	public JIWorld(int seed) {
@@ -13,6 +18,33 @@ public class JIWorld {
 	
 	public JIWorld(char Fx, char Fy, char Sx, char Sy) {
 
+	}
+	
+	private void genWorld(int seed = 0) {
+		if(seed == 0) Random theGen = new Random();
+		else Random theGen = new Random(seed);
+		
+		char x = (char)theGen.nextInt(100);
+		char y = (char)theGen.nextInt(100);
+		
+		map[x][y] = '+';
+		
+		char x = (char)theGen.nextInt(100);
+		char y = (char)theGen.nextInt(100);
+		
+		map[x][y] = '*';
+		
+	}
+	
+	public String toString() {
+		String toOut = "";
+		for(int i = 0; i < 100; i++) {
+			for(int j = 0;j<100;j++) {
+				toOut += map[i][j];
+			}
+			toOut += "\r\n";
+		}
+		return toOut;
 	}
 
 }
